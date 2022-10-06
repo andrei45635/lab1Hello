9cf79d4db8f42b52025480b09818414528bf828a
Andrei <91070254+andrei45635@users.noreply.github.com>
2022-10-06 09:44:23 +0300

    first lab

--- src/smallManagement.java
+++ src/smallManagement.java
@@ -17,6 +17,25 @@ class Employee{
     public void setPosition(String position) {
         Position = position;
     }
+
+    public int getSalary(){
+        return (Salary);
+    }
+    public void showEmployee(){
+        System.out.println("Employee " + Name + " has a salary of " + Salary + " is a " + Position + " and is " + Age + " years old");
+    }
+}
+
+class Manager extends Employee{
+    public Manager(String empName) {
+        super("Manager " + empName);
+    }
+    public void setManagerSalary(int bonus){
+        Salary = this.getSalary() + bonus;
+    }
+    public void showSalaryManager(){
+        System.out.println("The manager's salary after the bonus is: " + Salary);
+    }
 }
 
 public class smallManagement {
@@ -29,5 +48,12 @@ public class smallManagement {
         emp2.setAge(18);
         emp2.setPosition("VP");
         emp2.setSalary(9999);
+        emp1.showEmployee();
+        emp2.showEmployee();
+        Manager m1 = new Manager("Sandu Bogdan");
+        m1.setSalary(25);
+        m1.setManagerSalary(5);
+        m1.showSalaryManager();
+        m1.showEmployee();
     }
 }
